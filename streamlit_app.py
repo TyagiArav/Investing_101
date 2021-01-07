ac374ca53e14cc8286904e74b40f847399afeff8
xtreme243 <53234687+xtreme243@users.noreply.github.com>
2021-01-07 18:19:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,14 @@
 import streamlit as st
 import yfinance as yf
+import pandas as pd
 
-selections = ("GOOGL", "APPL", "AMZN")
-tickerSymbol = st.selectbox(selections)
-
+df = pd.DataFrame({
+  'stock': ["GOOGL", "APPL", "AMZN"],
+})
+tickerSymbol = st.selectbox("What is the stock your looking for", df['stock'])
 tickerdata = yf.Ticker(tickerSymbol)
 
 tickerDf = tickerdata.history(period='1d', start='2020-1-1')
 
 st.line_chart(tickerDf.Close)
 st.line_chart(tickerDf.Volume)
-'Web app Still in progress'
-'More to come!'
